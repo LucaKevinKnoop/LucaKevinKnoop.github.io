f428a5c243414f61bdf2009ab1b3cef2de9bd6ce
jannesulbrich <ulbrich.jannes@gmx.de>
2021-12-15 16:13:35 +0100

    new

--- drogen/scripts/menu.js
+++ drogen/scripts/menu.js
@@ -14,3 +14,41 @@ buttonTag.addEventListener('click', function () {
     toggleMenu = false;
   }
 });
+
+
+// ***FARBSPEKTAKEL der Drogenheadline***
+// headline auswählen:
+const drogenHead1Tag = document.querySelector('h1.drogen-headline1');
+const drogenHead2Tag = document.querySelector('h1.drogen-headline3');
+
+// Interval spielt function "drogenColor" alle 300ms ab
+setInterval(drogenColor, 300);
+
+// Variable fürs Hochzählen innerhalb der function:
+let varDrogenColor = 1;
+
+// Eigentliche Funktion:
+// Schleife, die die vier Farben nacheinander durchgeht:
+function drogenColor() {
+  if (varDrogenColor == 1) {
+    drogenHead1Tag.style.color = "#C8EE70";
+    drogenHead2Tag.style.color = "#C8EE70";
+    varDrogenColor = 2;
+  }
+  else if (varDrogenColor == 2) {
+    drogenHead1Tag.style.color = "#C76FF8";
+    drogenHead2Tag.style.color = "#C76FF8";
+    varDrogenColor = 3;
+  }
+  else if (varDrogenColor == 3) {
+    drogenHead1Tag.style.color = "#05BFAF";
+    drogenHead2Tag.style.color = "#05BFAF";
+    varDrogenColor = 4;
+  }
+  else {
+    drogenHead1Tag.style.color = "#FA6458";
+    drogenHead2Tag.style.color = "#FA6458";
+    varDrogenColor = 1;
+  }
+};
+// ***FARBSPEKTAKEL der Drogenheadline***
